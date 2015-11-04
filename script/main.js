9d80e1ba8d77eecc38bd0f6de75c6fcfcaa559bc
kingli <1258093480@qq.com>
2015-11-04 16:46:35 +0800

    添加功能表事件

--- script/main.js
+++ script/main.js
@@ -3,23 +3,35 @@ $(document).ready(function () {
 		$window = $(window),
 		$joinBtn = $('#joinBtn'),
 		$joinInput = $('#joinInput'),
-		$login=$('div.login'),
-		$main=$('div.main');
-		(function init() {
-			$window.keydown(function (event) {
-				/*按键没有特殊的按键的影响*/
-				if (!(event.ctrlKey || event.metaKey || event.altKey)) {
-					$joinInput.focus();
-				}
-				if (event.which === 13) {
-					USERNAME = cleanInput($joinInput.val());
-					$login.fadeOut(1000,function(){
-						$login.css({display:'none'});
-						$main.fadeIn(500);
+		$login = $('div.login'),
+		$main = $('div.main'),
+		$handlePenle=$('ul.handle-panel');
+	var handles = {
+
+	};
+	(function init() {
+		$window.keydown(function (event) {
+			/*按键没有特殊的按键的影响*/
+			if (!(event.ctrlKey || event.metaKey || event.altKey)) {
+				$joinInput.focus();
+			}
+			if (event.which === 13) {
+				USERNAME = cleanInput($joinInput.val());
+				$login.fadeOut(1000, function () {
+					$login.css({
+						display: 'none'
 					});
-				}
-			});
-		})();
+					$main.fadeIn(500);
+					initChatRoom();
+				});
+			}
+		});
+	})();
+	function initChatRoom(){
+		$handlePenle.on('click','li',function(event){
+			console.log(event.target);
+		});
+	}
 	//用于清除危险字符用的
 	function cleanInput(input) {
 		return $('<div/>').text(input).text();
